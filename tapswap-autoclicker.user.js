b9d7f178d05b7b45c4da64c28b25a481bee8b481
S3C237 <s3c237@gmail.com>
2024-07-12 00:15:33 +0330

    Update tapswap-autoclicker.user.js

--- tapswap-autoclicker.user.js
+++ tapswap-autoclicker.user.js
@@ -12,18 +12,18 @@
 // @homepage     https://github.com/S3C237/TapSwap
 // ==/UserScript==
 
-// Настраиваемые значения
-const minClickDelay = 30; // Минимальная задержка между кликами в миллисекундах
-const maxClickDelay = 50; // Максимальная задержка между кликами в миллисекундах
-const pauseMinTime = 100000; // Минимальная пауза в миллисекундах (100 секунд)
-const pauseMaxTime = 300000; // Максимальная пауза в миллисекундах (300 секунд)
-const energyThreshold = 25; // Порог энергии, ниже которого делается пауза
-const checkInterval = 1500; // Интервал проверки наличия монеты в миллисекундах (3 секунды)
-const maxCheckAttempts = 3; // Максимальное количество попыток проверки наличия монеты
+// Custom values
+const minClickDelay = 30; // Minimum delay between clicks in milliseconds
+const maxClickDelay = 50; // Maximum delay between clicks in milliseconds
+const pauseMinTime = 100000; // Minimum pause in milliseconds (100 seconds)
+const pauseMaxTime = 300000; // Maximum pause in milliseconds (300 seconds)
+const energyThreshold = 25; // Energy threshold below which a pause is made
+const checkInterval = 1500; // Coin presence check interval in milliseconds (3 seconds)
+const maxCheckAttempts = 3; // Maximum number of attempts to check for coin availability
 
-let checkAttempts = 0; // Счётчик попыток проверки
+let checkAttempts = 0; // Verification attempt counter
 
-// Конфигурация стилей для логов
+// Configuring styles for logs
 const styles = {
     success: 'background: #28a745; color: #ffffff; font-weight: bold; padding: 4px 8px; border-radius: 4px;',
     starting: 'background: #8640ff; color: #ffffff; font-weight: bold; padding: 4px 8px; border-radius: 4px;',
@@ -32,7 +32,7 @@ const styles = {
 };
 const logPrefix = '%c[TapSwapBot] ';
 
-// Перезапись функции console.log для добавления префикса и стилей
+// Rewriting console.log function to add prefix and styles
 const originalLog = console.log;
 console.log = function () {
     if (typeof arguments[0] === 'string' && arguments[0].includes('[TapSwapBot]')) {
@@ -40,10 +40,10 @@ console.log = function () {
     }
 };
 
-// Отключение остальных методов консоли для чистоты вывода
+// Disabling other console methods for clear output
 console.error = console.warn = console.info = console.debug = () => { };
 
-// Очистка консоли и стартовые сообщения
+// Clearing the console and startup messages
 console.clear();
 console.log(`${logPrefix}Starting`, styles.starting);
 console.log(`${logPrefix}Created by https://t.me/mudachyo`, styles.starting);
@@ -59,7 +59,7 @@ function getRandomCoordinateInCircle(radius) {
     do {
         x = Math.random() * 2 - 1;
         y = Math.random() * 2 - 1;
-    } while (x * x + y * y > 1); // Проверяем, что точка находится внутри круга
+    } while (x * x + y * y > 1); // Checking that the point is inside the circle
     return {
         x: Math.round(x * radius),
         y: Math.round(y * radius)
